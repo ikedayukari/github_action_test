4e26c1f7eebd931cf4c0b607d3adf8dfbd62074a
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-20 18:19:47 +0900

    aaaaaaaaaa

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -3,3 +3,16 @@
 for commit in ${COMMITS} ; do
     echo "${commit}"
 done
+
+#!/bin/bash
+
+messages=$(jq -r '
+    (.[] | [.message, .author.name, .timestamp, .url])
+    | @tsv
+' "${COMMITS}" | 
+{
+    while IFS=$'\t' read -r message name timestamp url; do
+          printf "[info][title]%s\n%s(%s)[/title]FILE_LIST[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$url"
+    done
+})
+echo "${messages}"
