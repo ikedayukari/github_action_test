4ee101a89843971fa9d945eb7a57bfa5b5511d6b
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-21 12:16:32 +0900

    aaaaa

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -1,7 +1,8 @@
 #!/usr/bin/env bash
 
 before_id=${BEFORE_ID}
-messages=$(echo "${COMMITS}" | jq -r '
+
+echo "message=$(echo "${COMMITS}" | jq -r '
     (.[] | [.message, .author.name, .timestamp, .id, .url])
     | @tsv
 ' | 
@@ -11,11 +12,10 @@ messages=$(echo "${COMMITS}" | jq -r '
         if [ 1 -lt "$parent_count" ]; then
             continue
         fi
-        added_files=$(git diff --diff-filter=A --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed "s/^/added：/g")
-        removed_files=$(git diff --diff-filter=R --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed "s/^/removed：/g")
-        modified_files=$(git diff --diff-filter=M --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed "s/^/modified：/g")
+        added_files=$(git diff --diff-filter=A --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed 's/^/added:/g')
+        removed_files=$(git diff --diff-filter=R --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed 's/^/removed:/g')
+        modified_files=$(git diff --diff-filter=M --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed 's/^/modified:/g')
         printf "[info][title]%s\n%s(%s)[/title]%s%s%s[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$added_files" "$removed_files" "$modified_files" "$url"
         before_id=${id}
     done
-})
-echo "${messages}"
+})" >> "$GITHUB_OUTPUT"
--- .github/workflows/message.sh
+++ .github/workflows/message.sh
@@ -1,9 +1,9 @@
 #!/usr/bin/env bash
 
+if [ -z "${MESSAGE}" ]; then
+   exit 0
+fi
+
 curl -s -X POST "https://api.chatwork.com/v2/rooms/${ROOM_ID}/messages" \
     -H "X-ChatWorkToken: ${ABCDEF}" \
-    -d @- <<EOS
-body=[info][title]${MESSAGE}
-${AUTHOR}(${TIMESTAMP})[/title]
-FILE_LIST[/info]
-EOS
+    -d "body=${MESSAGE}"
