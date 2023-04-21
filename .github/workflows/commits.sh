bfd47451d3f8b1c4042b2095b15bb33509f0b81e
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-21 16:43:32 +0900

    aaa

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -11,9 +11,9 @@ message=$(echo "$COMMITS" | jq -r '
         if [ 1 -lt "$parent_count" ]; then
             continue
         fi
-        added_files=$(git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added:/g')
-        removed_files=$(git diff --diff-filter=D --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed:/g')
-        modified_files=$(git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified:/g')
+        added_files=$(git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added : /g')
+        removed_files=$(git diff --diff-filter=D --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed : /g')
+        modified_files=$(git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified : /g')
         printf "[info][title]%s\n%s(%s)[/title]%s%s%s[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$added_files" "$removed_files" "$modified_files" "$url"
     done
 })
