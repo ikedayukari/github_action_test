82acf32deae09a33abef3d99ef6ea5daacfa3303
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-21 10:21:16 +0900

    aaaaaa

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -7,7 +7,7 @@ messages=$(echo "${COMMITS}" | jq -r '
 ' | 
 {
     while IFS=$'\t' read -r message name timestamp id url; do
-        modified_files=$(git diff --diff-filter=M --name-only "${before_id}".."${id}")
+        modified_files=$(git diff --diff-filter=M --name-only "${before_id}".."${id}" | sed "s/^/Added：/g")
         echo "${modified_files}"
         printf "[info][title]%s\n%s(%s)[/title]FILE_LIST[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$url"
         before_id=${id}
