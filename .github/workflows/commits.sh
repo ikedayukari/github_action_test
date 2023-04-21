3c99af3533887c8696c4ba4af88c484c1f8b54ec
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-21 15:53:15 +0900

    abc

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -1,28 +1,26 @@
 #!/usr/bin/env bash
 
-before_id=${BEFORE_ID}
-
-message=$(echo "${COMMITS}" | jq -r '
+message=$(echo "$COMMITS" | jq -r '
     (.[] | [.message, .author.name, .timestamp, .id, .url])
     | @tsv
 ' | 
 {
     while IFS=$'\t' read -r message name timestamp id url; do
-        parent_count=$(git cat-file -p "${id}" | grep -c '^parent')
+        parent_id=$(git cat-file -p "$id" | grep '^parent' | sed 's/^parent //g')
+        parent_count=$(echo "$parent_id" | grep -c '')
         if [ 1 -lt "$parent_count" ]; then
             continue
         fi
-        added_files=$(git diff --diff-filter=A --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed 's/^/added:/g')
-        removed_files=$(git diff --diff-filter=R --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed 's/^/removed:/g')
-        modified_files=$(git diff --diff-filter=M --name-only "${before_id}".."${id}" ':!/ignore_directory/*' | sed 's/^/modified:/g')
+        added_files=$(git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added:/g')
+        removed_files=$(git diff --diff-filter=R --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed:/g')
+        modified_files=$(git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified:/g')
         printf "[info][title]%s\n%s(%s)[/title]%s%s%s[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$added_files" "$removed_files" "$modified_files" "$url"
-        before_id=${id}
     done
 })
 
 delimiter="EOF$(openssl rand -hex 8)"
 {
-  echo "message<<${delimiter}"
+  echo "message<<$delimiter"
   echo "$message"
-  echo "${delimiter}"
+  echo "$delimiter"
 } >> "$GITHUB_OUTPUT"
