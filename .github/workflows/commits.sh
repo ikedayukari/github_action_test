e6c944e7cde5da4d5b7191a09782f47670664aeb
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-21 16:41:09 +0900

    aaaaa

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -12,7 +12,7 @@ message=$(echo "$COMMITS" | jq -r '
             continue
         fi
         added_files=$(git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added:/g')
-        removed_files=$(git diff --diff-filter=R --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed:/g')
+        removed_files=$(git diff --diff-filter=D --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed:/g')
         modified_files=$(git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified:/g')
         printf "[info][title]%s\n%s(%s)[/title]%s%s%s[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$added_files" "$removed_files" "$modified_files" "$url"
     done
