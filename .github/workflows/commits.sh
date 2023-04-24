cf276b4b4eb5c4f715861f62e9ebceb2afe3ebc9
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-24 14:44:08 +0900

    Revert "aaa"
    This reverts commit f9aec23534b55792d6a30f1f0c6ec87172ca7e82.

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -8,18 +8,17 @@ message=$(echo "$COMMITS" | jq -r '
     while IFS=$'\t' read -r message name timestamp id url; do
         parent_id=$(git cat-file -p "$id" | grep '^parent' | sed 's/^parent //g')
         parent_count=$(echo "$parent_id" | grep -c '')
-        continue
-        # if [ 1 -lt "$parent_count" ]; then
-        #     continue
-        # fi
-        # files="$( ( 
-        #         (git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added : /g')
-        #         (git diff --diff-filter=D --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed : /g')
-        #         (git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified : /g')
-        #         (git diff --diff-filter=R --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/renamed : /g')
-        #         (git diff --diff-filter=C --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/copied : /g')
-        #     ) | sed '/^$/d')"
-        # printf "[info][title]%s\n%s(%s)[/title]%s[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$files" "$url"
+        if [ 1 -lt "$parent_count" ]; then
+            continue
+        fi
+        files="$( ( 
+                (git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added : /g')
+                (git diff --diff-filter=D --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed : /g')
+                (git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified : /g')
+                (git diff --diff-filter=R --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/renamed : /g')
+                (git diff --diff-filter=C --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/copied : /g')
+            ) | sed '/^$/d')"
+        printf "[info][title]%s\n%s(%s)[/title]%s[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$files" "$url"
     done
 })
 
