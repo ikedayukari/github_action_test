4270bfb65d0ca237c1bd4805b297071ed6174bdf
ikeda yukari <y-ikeda@nextninja.co.jp>
2023-04-24 12:02:47 +0900

    a

--- .github/workflows/commits.sh
+++ .github/workflows/commits.sh
@@ -11,11 +11,13 @@ message=$(echo "$COMMITS" | jq -r '
         if [ 1 -lt "$parent_count" ]; then
             continue
         fi
-        files="$( ( (git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added : /g')
-(git diff --diff-filter=D --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed : /g')
-(git diff --diff-filter=R --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/renamed : /g')
-(git diff --diff-filter=C --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/copied : /g')
-(git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified : /g') ) | sed '/^$/d')"
+        files="$( ( 
+                (git diff --diff-filter=A --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/added : /g')
+                (git diff --diff-filter=D --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/removed : /g')
+                (git diff --diff-filter=M --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/modified : /g')
+                (git diff --diff-filter=R --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/renamed : /g')
+                (git diff --diff-filter=C --name-only "$parent_id".."$id" ':!/ignore_directory/*' | sed 's/^/copied : /g')
+            ) | sed '/^$/d')"
         printf "[info][title]%s\n%s(%s)[/title]%s[hr]%s[/info]\n" "$message" "$name" "$timestamp" "$files" "$url"
     done
 })
